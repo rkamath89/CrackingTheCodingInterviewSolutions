30c47f461721b539608babe9644789db4ede5039
rkamath89 <rkamath@email.arizona.edu>
2015-06-08 19:10:25 -0700

    3.6 Sorting a Stack implemented
    Implemented as part of 3.2
    TODO : SORT IT IN PLACE

--- chapter3Ctci/src/chapter3Ctci/minimumStack.java
+++ chapter3Ctci/src/chapter3Ctci/minimumStack.java
@@ -34,18 +34,57 @@ public class minimumStack extends Stack
 				minStack.push(val);
 			}
 		}
+		actualStack.currentSize++;
 		actualStack.push(val);
 
 	}
 	int popMin()
 	{
 		int value =-1;
+		actualStack.currentSize--;
 		value = actualStack.pop();
 		if(value == getMin())
 		{
 			minStack.pop();
 		}
 		return value;
+	}
+	void sortStack()
+	{
+		Stack tempStack = new Stack();
+		int tempVal = -1;
+		while(actualStack.currentSize != 0)
+		{
+			actualStack.currentSize--;
+			tempVal = actualStack.pop();
+			if(tempVal  >= tempStack.peek()|| tempStack.peek() == 999)
+			{
+				tempStack.currentSize++;
+				tempStack.push(tempVal);
+			}
+			else 
+			{
+				int count = 0;// To keep track of number of elements popped
+				while(tempVal < tempStack.peek())
+				{
+					count++;
+					actualStack.currentSize++;
+					tempStack.currentSize--;
+					actualStack.push(tempStack.pop());
+				}
+				tempStack.currentSize++;
+				tempStack.push(tempVal);
+				while(count > 0)
+				{
+					count--;
+					tempStack.currentSize++;
+					actualStack.currentSize--;
+					tempStack.push(actualStack.pop());
+				}
+			}
+		}
+		System.out.println(" Sorted Stack is :: ");
+		tempStack.printQueue(tempStack.top);
 	}
 	public static void main(String[] args) 
 	{
@@ -58,6 +97,7 @@ public class minimumStack extends Stack
 			System.out.println("2: Pop from Queue");
 			System.out.println("3: Print Elements in Queue");
 			System.out.println("4: Find Min of Queue");
+			System.out.println("5: Sort Stack");
 			int choice = input.nextInt();
 			switch(choice)
 			{
@@ -83,6 +123,9 @@ public class minimumStack extends Stack
 			case 4:
 				int minVal =minStack.getMin();
 				System.out.println("Min of Queue is : "+minVal);
+				break;
+			case 5:
+				minStack.sortStack();
 				break;
 			}
 		}
